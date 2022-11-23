5eb7a43efd280ccce2345e53f239cb29fa567d62
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-11-23 15:14:11 +0100

    finalizing the script

---
+++ datascript.sql
@@ -0,0 +1,31 @@
+INSERT INTO person (person_ID,firstName,lastName,phone,email,personalNumber)
+VALUES
+  ('C63BC4CB-17B7-AC87-B913-33C4A9C91189','Aaron','Pollard','072-877-5614','neque.nullam@icloud.com','1984-34-29-5486'), -- student
+  ('47AA8F7B-D751-2AD1-7270-C443DCF1E115','Ocean','Guzman','072-450-6131','massa.integer@protonmail.ca','1957-67-17-4664'), -- student
+  ('BCA07B0A-7F1A-2E47-F72A-07B5A880E9D5','Victoria','Freeman','072-618-3888','dictum.phasellus@yahoo.net','1903-39-67-5738'), -- student
+  ('E596BEDA-68AD-C941-8815-D97EEA38BDAD','Kameko','Ayers','072-151-9663','sem.molestie.sodales@hotmail.ca','1988-14-81-6785'), -- student
+  ('4AA0A9BC-DDB9-9EE5-64C4-55FF6DDFD541','Aaron','Roberts','072-566-0057','rhoncus.nullam@protonmail.com','1991-54-70-3752'), -- student
+  ('C9B71F4E-238B-1E52-8521-2DE0880F56F7','Ruth','Rivers','072-583-8183','quisque@aol.ca','1964-37-95-5272'), -- student
+  ('1EEC97FD-3B7A-FA75-38DD-6C92B875A06C','Amelia','Parsons','072-866-3418','eu.neque.pellentesque@outlook.couk','1935-71-04-7524'), -- student
+  ('1C338AA3-C202-F05A-CBF3-1249DC5E8727','Althea','Combs','072-038-7398','ultricies@icloud.couk','1951-36-53-0283'), -- student
+  ('C53D4467-679D-C736-69AD-07DA7EBEA668','Ian','Grant','072-864-6674','montes@hotmail.net','1946-37-46-7690'), -- student
+  ('3D5C6015-5E11-74B9-9B66-92EDD5CA88B5','Chandler','Peters','072-140-5322','donec.nibh@aol.edu','1956-19-25-8285'), -- student
+  ('3A24DAB9-2061-FA46-3111-4A1E6767463F','Quemby','Meadows','072-704-3492','est.vitae.sodales@protonmail.net','1922-49-88-7735'), -- student
+  ('64B69F1A-1AA2-1C47-A334-6D47DCB3B386','Mia','Acevedo','072-537-8282','phasellus.ornare@aol.org','1918-03-51-3835'), -- student
+  ('35A7882C-DB53-6D2A-A753-1E43123B5F9B','Jasper','Serrano','072-012-5527','pretium.neque@yahoo.couk','1991-91-32-7448'), -- student
+  ('5FD9AD7B-0241-9E52-31DB-0CAC2F37CE27','Jillian','Sims','072-335-1223','duis.gravida.praesent@hotmail.edu','1972-04-62-6766'), -- contact person
+  ('2D248A73-1FAA-3A71-97A4-E34C11575361','Rhea','Charles','072-633-1782','ut@aol.ca','1944-57-62-8838'), -- contact person
+  ('3CCB63CC-7939-756B-D7B4-38D27FF31153','Cullen','Maxwell','072-453-9278','phasellus.libero@protonmail.ca','1903-39-26-5467'), -- contact person
+  ('17BF4593-E57B-E9D6-5434-24B5668B11D7','Hilary','Harvey','072-113-4693','quisque.imperdiet@outlook.com','1946-41-26-1174'), -- instructor
+  ('DA7289AF-2F23-0AD2-5845-5FBA7540954E','Judah','Duffy','072-151-8709','vitae@google.ca','1942-76-49-3555'), -- instructor
+  ('47A322F9-27BC-28B2-0B45-716DA676FA8F','Eric','Shelton','072-398-3616','id.risus.quis@yahoo.org','1952-42-58-8702'), -- instructor
+  ('B21CD117-6565-E944-E8AE-A6544819B44D','Angela','Frank','072-664-4751','in.consectetuer@outlook.couk','1946-56-73-5619'); -- instructor
+
+  INSERT INTO instructor (instructor_ID,ensemble,instrumentType)
+VALUES
+  ('17BF4593-E57B-E9D6-5434-24B5668B11D7','false','bass'),
+  ('DA7289AF-2F23-0AD2-5845-5FBA7540954E','true','drums'),
+  ('47A322F9-27BC-28B2-0B45-716DA676FA8F','true','bass'),
+  ('B21CD117-6565-E944-E8AE-A6544819B44D','false','guitar'),
+
+
