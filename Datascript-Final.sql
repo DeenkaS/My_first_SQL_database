74a0d5f7c81d92fb113c9062fc75a39c301de3ec
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-11-29 10:29:57 +0100

    doing the data

---
+++ Datascript-Final.sql
@@ -0,0 +1,21 @@
+INSERT INTO person (person_ID,firstName,lastName,phone,email,personalNumber)
+VALUES
+("1","Jonas","Dåderman","0721234567","jonasdad@mail.se","200001011234" )
+("2","Deniel","Saskin","0729185149","Saskin@kth.se","199208138011")
+("3","Ingemar","Gerling","0700010001","gerling@mail.se","201002291432")
+("4","Stephan","Persson","0733334444","Stephan@gmail.com","201003141230")
+("5","Olle","Berlin","0731230095","Berliner@mail.se","197003205868")
+("6","Rove","Wierup","0735556666","28cm@hotmail.com","200002021234")
+("7","Tessla","Wownörd","0700940944","azeroth@mail.se","199908230001")
+("8","Isabelle","Forsberg","0734056767","forsberg@hockey.se","199906051245")
+("9","Peter","Otrevlig","0737776666","dryg@hockey.se","190006051245")
+("10","Oscar","Nyman","0733458876","nyman@hockey.se","199912121245")
+("11","Jennifer","Svensk","0739876676","js@hockey.se","201206051245")
+
+ INSERT INTO instructor (instructor_ID,ensemble,person_ID)
+VALUES
+("I1","true","1")
+("I2","false","2")
+("I3","true","8")
+("I4","false","5")
+
