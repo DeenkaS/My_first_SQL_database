3d61db3aba0e8bf6ebf6f87bc04b7dfbe0507a5c
DeenkaS <96053994+DeenkaS@users.noreply.github.com>
2022-12-01 09:25:55 +0100

    lets go

--- Datascript-Final.sql
+++ Datascript-Final.sql
@@ -1,18 +1,20 @@
-INSERT INTO person (person_ID,firstName,lastName,phone,email,personalNumber)
+INSERT INTO person (firstName,lastName,phone,email,personalNumber)
 VALUES
-(1,'Jonas','Dåderman','0721234567','jonasdad@mail.se','200001011234'),
-(2,'Deniel','Saskin','0729185149','Saskin@kth.se','199208138011'),
-(3,'Ingemar','Gerling','0700010001','gerling@mail.se','201002291432'),
-(4,'Stephan','Persson','0733334444','Stephan@gmail.com','201003141230'),
-(5,'Olle','Berlin','0731230095','Berliner@mail.se','197003205868'),
-(6,'Rove','Wierup','0735556666','28cm@hotmail.com','200002021234'),
-(7,'Tessla','Wownörd','0700940944','azeroth@mail.se','199908230001'),
-(8,'Isabelle','Forsberg','0734056767','forsberg@hockey.se','199906051245'),
-(9,'Peter','Otrevlig','0737776666','dryg@hockey.se','190006051245'),
-(10,'Oscar','Nyman','0733458876','nyman@hockey.se','199912121245'),
-(11,'Jennifer','Svensk','0739876676','js@hockey.se','201206051245'),
-(12, 'Mamma', 'Svensk', '0701917151', 'mammaS@mail.se', '197201121268'),
-(13, 'Bobby', 'Kotick', '0762095551', 'Kotick@mail.se', '200506171238');
+('Jonas','Dåderman','0721234567','jonasdad@mail.se','200001011234'),
+('Deniel','Saskin','0729185149','Saskin@kth.se','199208138011'),
+('Ingemar','Gerling','0700010001','gerling@mail.se','201002291432'),
+('Stephan','Persson','0733334444','Stephan@gmail.com','201003141230'),
+('Olle','Berlin','0731230095','Berliner@mail.se','197003205868'),
+('Rove','Wierup','0735556666','28cm@hotmail.com','200002021234'),
+('Tessla','Wownörd','0700940944','azeroth@mail.se','199908230001'),
+('Isabelle','Forsberg','0734056767','forsberg@hockey.se','199906051245'),
+('Peter','Otrevlig','0737776666','dryg@hockey.se','190006051245'),
+('Oscar','Nyman','0733458876','nyman@hockey.se','199912121245'),
+('Jennifer','Svensk','0739876676','js@hockey.se','201206051245'),
+('Mamma', 'Svensk', '0701917151', 'mammaS@mail.se', '197201121268'),
+('Bobby', 'Kotick', '0762095551', 'Kotick@mail.se', '200506171238');
+
+--insert into student(person_id) VALUES ((SELECT person_id from person));
 
 INSERT INTO instructor (instructor_ID,ensemble,person_ID)
 VALUES
