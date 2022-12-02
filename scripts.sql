4b3280e032d0b7eaf1a6bbd94483136bf3401b01
Jonas Dåderman <jonasdad@kth.se>
2022-12-02 09:12:06 +0100

    working script 1

--- scripts.sql
+++ scripts.sql
@@ -1,10 +1,10 @@
 --Returns number of each lesson type held per month for a specified year, as well as the total number of lessons during that month.
 --counts # lessontypes
+
 select extract(month from lessondate) as month, 
-sum(case when lessontype = 'single' then 1 else null end) as single,
-sum(case when lessontype = 'group' then 1 else null end) as group,
-sum(case when lessontype != 'group' and lessontype != 'single' then 1 else null end) as ensemble,
-from lesson group by month;
+sum(case when lessontype = 'single' then 1 else null end) as single, 
+sum(case when lessontype = 'group' then 1 else null end) as group, 
+sum(case when lessontype != 'group' and lessontype != 'single' then 1 else null end) as ensemble from lesson group by month;
 --Show number of siblings of each student.
 
 
