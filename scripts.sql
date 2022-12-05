63e6a9f12bd53959b571244665e54bad9c5fda35
DeenkaS <96053994+DeenkaS@users.noreply.github.com>
2022-12-05 11:46:30 +0100

    fixed comments

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,4 @@
---Returns number of each lesson type held per MONTH for a specified year, as well as the total number of lessons during that MONTH.
+--Returns number of each lesson type held per month for a specified year, as well as the total number of lessons during that month.
 
 SELECT TO_CHAR(lessondate, 'MON') AS MONTH, 
     SUM(CASE WHEN lessontype = 'single' THEN 1 ELSE 0 END) AS "single lessons", 
@@ -13,7 +13,7 @@ SELECT student.student_id ,COUNT(siblings) AS "Number of Siblings"
     FROM (SELECT student_id FROM sibling UNION ALL SELECT student_id_2 FROM sibling) AS siblings 
         FULL JOIN student ON siblings.student_id=student.student_id GROUP BY siblings, student.student_id ORDER BY sibling DESC;
 
---returns number of instructors given more than X lessons during the current MONTH.
+--returns number of instructors given more than X lessons during the current month.
 CREATE VIEW instructor_lessons_per_month AS
     SELECT instructor_ID, COUNT(instructor_id) AS lessons_given FROM lesson 
         WHERE EXTRACT(MONTH FROM lessondate) = EXTRACT(MONTH FROM CURRENT_DATE)
@@ -21,7 +21,7 @@ CREATE VIEW instructor_lessons_per_month AS
                 HAVING COUNT(instructor_id) >= 1
                     ORDER BY lessons_given DESC;
 
---show ensemble next week AND available slots sorted BY music AND day
+--show ensemble next week and available slots sorted BY music and day
 SELECT lesson.maxstudents - COUNT(attending_students) AS availableSlots, lessontype AS genre, TO_CHAR(lessondate, 'Day') AS day
     FROM lesson full join attending_students on lesson.lesson_id = attending_students.lesson_id
         WHERE lessontype != 'single' AND lessontype != 'group' AND extract(week FROM lessondate) = extract(week FROM current_date) + 1 
